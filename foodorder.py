593990eb0e1582f0748d60bfe6d9411398102e83
rajat <rajatkhatri0002@gmail.com>
2020-07-05 15:19:44 +0530

    taking user input for order

--- foodorder.py
+++ foodorder.py
@@ -8,9 +8,21 @@ menu_item4 = MenuItem('Orange Juice', 2)
 menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
 
 
-
-
 for index,menu_item in enumerate(menu_items):
     # Print out in the format '0. Sandwich: $5' for each index
     print(str(index)+". "+menu_item.info())
 
+print('--------------------')
+
+order = int(input('Enter menu item number: '))
+selected_menu = menu_items[order]
+print('Selected item: ' + selected_menu.name)
+
+# Receive input from the console for no of items
+count = int(input("Enter quantity (10% off for 3 or more): "))
+
+# Call the get_total_price method
+result = selected_menu.get_total_price(count)
+
+# Output 
+print("Your total is $"+str(result))
