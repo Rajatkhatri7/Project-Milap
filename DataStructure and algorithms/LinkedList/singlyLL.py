49e9ccb090bb570807dfae9cee69956452ad92cc
rajat <rajatkhatri0002@gmail.com>
2020-07-11 03:20:02 +0530

    inser after optimised

--- DataStructure and algorithms/LinkedList/singlyLL.py
+++ DataStructure and algorithms/LinkedList/singlyLL.py
@@ -23,9 +23,46 @@ class LinkedList:
             last_node.next = new_node
             new_node.next = None
 
+    def prepend(self,data):
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+            new_node.next = None
+            return
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+
+
+    def insert_after(self,target_node,data):
+        ptr = self.head
+        while ptr: 
+            if target_node not in ptr.data: 
+                return
+            else:
+
+                new_node = Node(data)
+
+                new_node.next = ptr.next
+                ptr.next = new_node
+            ptr = ptr.next
+
+
+
     def print_list(self):
         ptr = self.head
         while ptr:
             print(ptr.data)
             ptr = ptr.next
 
+
+llist = LinkedList()
+llist.appending("A")
+llist.appending("B")
+llist.appending("C")
+
+
+llist.insert_after("m", "D")
+
+llist.print_list()  
